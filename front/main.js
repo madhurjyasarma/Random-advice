cb599eafffa612c8a6cdc4b2e828697a3500588a
Madhurjya Sarma <77984764+madhurjyasarma@users.noreply.github.com>
2021-12-23 19:13:26 +0530

    advice fetch API done

--- front/main.js
+++ front/main.js
@@ -1,4 +1,5 @@
-$('body').terminal({
+$('body').terminal(
+{
     iam: function (name) {
         this.echo('Hello, ' + name +
             '. Welcome to TERMINAL UI.');
@@ -12,6 +13,29 @@ $('body').terminal({
           'iam <your name>   :: for greeting\n'
         + 'developer         :: to know the Anonymous\n');
     },
-}, {
+    hie: function (name) {
+        this.echo(saysomething(name));
+    },
+    advice: function () {
+        this.echo(fetchAdviceAPI());
+    }
+},
+{
     greetings: 'Terminal UI - Hack NOSA :: Type \'help\' to show options'
 });
+
+function saysomething(name) {
+    return 'Hieee ' +name+ '..Weocooooooooooooomeeeeeeeeeeeeeeeee from saysomething.';
+}
+
+async function fetchAdviceAPI() {
+    let url = "https://api.adviceslip.com/advice";
+    let response = await fetch(url);
+    // console.log(response)
+    let data = await response.text();
+    // console.log(data);
+    parsed_response = JSON.parse(data);
+    // console.log(parsed_response);
+    // console.log(parsed_response.slip.advice);
+    return parsed_response.slip.advice
+}
